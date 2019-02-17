f25f7fe85b0675a4c7f131326d4aa6a5ae265165
rileyhoff <rhoff@A-Higher-Power.localdomain>
2019-02-16 20:15:09 -0500

    add vertcal title on scroll

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //SQKZ portfolio
 //created by: Riley Hoff
+
+//vertical title on scroll
+function verticalTitle() {
+    var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if (size == 0 && scroll > endOfDocumentTop) {
+        title.className = 'verticalTitle';
+        size = 1;
+    } else if (size == 1 && scroll <= endOfDocumentTop) {
+        title.className = 'horizontalTitle';
+        size = 0;
+    }
+}
